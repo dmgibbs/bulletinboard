89d43e7db98937be738e4ddf72897382f473aa0c
Damion Gibbs <dmgibbs@writeme.com>
2019-01-09 03:09:33 +0000

    added ability to edit/remove notes

--- src/Note.js
+++ src/Note.js
@@ -1,21 +1,56 @@
 import React, {Component} from  'react'
 import {FaPencilAlt} from 'react-icons/fa'
-import {FaTrash} from 'react-icons/fa'
+import {FaTrash , FaFloppy0} from 'react-icons/fa'
+
 
 class Note extends Component {
   constructor(props){
     super(props)
     this.edit = this.edit.bind(this)
+    this.state = {editing:false}
     this.remove = this.remove.bind(this)
+    this.renderForm = this.renderForm.bind(this)
+    this.renderDisplay = this.renderDisplay.bind(this)
   }
+
   edit(){
+    this.setState = {editing: true}
     alert("Editing note")
   }
   remove(){
     alert("removing it")
   }
 
-  render(){
+  renderForm(){
+    <form>
+      <textarea>   
+        <button> 
+          <Fafloppy0>
+          </Fafloppy0>
+        </button>.  
+      
+      </textarea> 
+    </form>
+
+  }
+
+  renderForm(){
+    return (
+      <div className = "Note">
+        <form>
+          <textarea />
+            <button> <FaFloppy0 />
+            </button>
+ 
+        </form>
+        
+      </div>
+    )
+  }
+
+
+
+  renderDisplay(){
     return (
       <div className = "note">
         <p> Learn React </p>
@@ -25,8 +60,18 @@ class Note extends Component {
         </span>
       </div>
     )
-
   }
+  render (){
+    if (this.state.editing){
+     return this.renderForm()
+    else {
+      return this.renderDisplay();
+    }
+  }
+}
+
+
+
 }
 
 export default Note;
